9b1d93dddf51cbae5f2fd3d761aa836ad7e7a72b
Itai Belotserkovski <itaibelo2904@gmail.com>
2022-12-21 19:01:46 +0200

    adding onSearch feature.

--- js/app.controller.js
+++ js/app.controller.js
@@ -8,6 +8,39 @@ window.onPanTo = onPanTo
 window.onGetLocs = onGetLocs
 window.onGetUserPos = onGetUserPos
 window.onDelete = onDelete
+window.onSearch = onSearch
+
+function onSearch(ev) {
+    if (ev) ev.preventDefault()
+    const elInputSearch = document.querySelector('input[name=search]')
+    const searchData = axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${elInputSearch.value}&key=AIzaSyB3YTMwlpvpzoH8xqyIROJIRqq4hG_RtyM`)
+    // const lat = searchData.data.results.geometry.location.lat
+    // const lng = searchData.data.results.geometry.location.lng
+    searchData.then(loc => {
+        // loc.data
+        const lat = (loc.data.results[0].geometry.location.lat)
+        console.log(lat)
+        const lng = (loc.data.results[0].geometry.location.lng)
+        console.log(lng)
+        centerUserPos({ lat: lat, lng: lng })
+    })
+    // }).then(loc => {
+    //     // loc.data
+    //     console.log(loc.results)
+    // })
+    // .then(loc => {
+    //     // loc.data
+    //     console.log(loc.results)
+    // })
+
+    // console.log(lat)
+    // console.log(lng)
+    // locService.getLocs(elInputSearch.value)
+    //     .then(locs => {
+    //         if (!locs.length) return
+    //         // playVideo(videos[0].id)
+    //     })
+}
 
 function onInit() {
     mapService.initMap()
